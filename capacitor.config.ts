268bbb878307ff9a959cd120f5d1fac799967403
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2024-04-15 20:00:52 +0530

    test

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,10 +11,15 @@ const config: CapacitorConfig = {
   plugins: {
     GoogleAuth: {
       scopes: ['profile', 'email'],
-      serverClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      serverClientId: '426738143380-n76sjua1ilsg3qsdd74c3kio79treh40.apps.googleusercontent.com',
       forceCodeForRefreshToken: true,
     },
   },
 };
 
+
+// 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
+
+// 426738143380-n76sjua1ilsg3qsdd74c3kio79treh40.apps.googleusercontent.com
+// 426738143380-fqf68i8mmmkqdc5u9ikdt41brtqvkbg0.apps.googleusercontent.com
 export default config;
